4f74cc1bed9c305e10e4a06eae5eb9f9a4ce0f84
ManojPanta <pantamanoj08@gmail.com>
2025-03-25 17:30:46 +0545

    1st Attempt on typescript

---
+++ src/app/chai/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button"
+
+
+
+function page() {
+  return (
+    <>
+    <div className="text-2xl font-white">Weloome to mY pAGE</div>
+    <Button className="bg-red-400"> Hello Hello </Button>
+    </>
+  )
+}
+
+export default page
